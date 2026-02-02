2cb8fd68e428f7d9bcaccfeb115a8e25e413574c
MartinFrank77 <martin.frank@irix.ch>
2026-02-02 16:56:49 +0100

    fixed routing issue

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
 
   <AuthProvider>
-    <BrowserRouter basename="/captain-mercer">
+    <BrowserRouter basename="/captain-mercer/">
       <App />
     </BrowserRouter>
   </AuthProvider>
